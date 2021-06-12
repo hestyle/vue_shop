a57c089cb9f5ad5d43cced5ff878afdd0a02a8ee
hestyle <hestyle@qq.com>
2021-06-12 16:14:06 +0800

    完成了登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,42 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/home',
+    component: Home
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    // 登录，直接放行
+    return next();
+  }
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token');
+  if (!tokenStr) {
+    // 未登录，跳转到登录页面
+    return next('/login');
+  }
+  // 已登录，放行
+  return next();
 })
 
 export default router
